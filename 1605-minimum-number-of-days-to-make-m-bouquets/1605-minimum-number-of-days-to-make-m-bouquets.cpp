91c885799660760266e81b1af0a6bb8ef9d90f26
PASAPALA BHUVANESH <83776861+Bhuvan028@users.noreply.github.com>
2025-07-15 22:36:23 +0530

    Time: 30 ms (19.99%), Space: 70.5 MB (76.59%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
+    // Return the number of maximum bouquets that can be made on day mid.
+    int getNumOfBouquets(vector<int>& bloomDay, int mid, int k) {
+        int numOfBouquets = 0;
 
-   bool check(vector<int>arr,int mid,int k,int m){
-        int count=0; int ans=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<=mid){
+        int count = 0;
+        for (int i = 0; i < bloomDay.size(); i++) {
+            // If the flower is bloomed, add to the set. Else reset the count.
+            if (bloomDay[i] <= mid) {
                 count++;
+            } else {
+                count = 0;
             }
-            else{
-                ans+=(count/k);
-                count=0;
+
+            if (count == k) {
+                numOfBouquets++;
+                count = 0;
             }
         }
-        ans+=count/k;
-        return ans>=m;
+
+        return numOfBouquets;
     }
-    int minDays(vector<int>& arr, int m, int k) {
-        int mini = INT_MAX, maxi = INT_MIN;
-    for (int i = 0; i < arr.size(); i++) {
-        mini = min(mini, arr[i]);
-        maxi = max(maxi, arr[i]);
-    }
-    int low=mini,high=maxi;
-    long long val = m * 1LL * k * 1LL;
-        if(val>arr.size()){
-            return -1;
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int start = 0;
+        int end = 0;
+        for (int day : bloomDay) {
+            end = max(end, day);
         }
-        while(low<=high){
-            int mid = (low+high)/2;
-            
-            if(check(arr,mid,k,m)){              
-                
-                 high = mid - 1;
+
+        int minDays = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
+                minDays = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else{
-               low = mid + 1;
-            }
-            
         }
-       return low;
+
+        return minDays;
     }
 };
