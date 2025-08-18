899b47a87595be091f2dfcb5b3230c02d9797339
PASAPALA BHUVANESH <83776861+Bhuvan028@users.noreply.github.com>
2025-08-18 22:57:38 +0530

    Time: 3 ms (54%), Space: 11 MB (63.88%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,7 +5,7 @@ public:
             return a.second>b.second;
         }
     string frequencySort(string s) {
-        vector<int>arr(256,0);
+        
         map<char,int>mp; string res="";
         for(int i=0;i<s.size();i++){
             mp[s[i]]++;
